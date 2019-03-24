574546df2baf54277ff88de4ef30215f03ca91f6
Eike Welk <eike.welk@gmx.net>
2019-03-25 00:09:05 +0100

    Better variable names and better comments.

--- mpu6500.py
+++ mpu6500.py
@@ -32,6 +32,7 @@ import time
 import Adafruit_GPIO.I2C
 # pylint: enable=import-error
 
+# Internal registers
 _GYRO_CONFIG = 0x1b
 _ACCEL_CONFIG = 0x1c
 _ACCEL_CONFIG2 = 0x1d
@@ -52,6 +53,7 @@ _GYRO_ZOUT_H = 0x47
 _GYRO_ZOUT_L = 0x48
 _WHO_AM_I = 0x75
 
+# Accelerometer sensitivity
 #_ACCEL_FS_MASK = 0b00011000
 ACCEL_FS_SEL_2G = 0b00000000
 ACCEL_FS_SEL_4G = 0b00001000
@@ -63,6 +65,7 @@ _ACCEL_SO_4G = 8192 # 1 / 8192 ie. 0.122 mg / digit
 _ACCEL_SO_8G = 4096 # 1 / 4096 ie. 0.244 mg / digit
 _ACCEL_SO_16G = 2048 # 1 / 2048 ie. 0.488 mg / digit
 
+# Gyroscope sensitivity
 #_GYRO_FS_MASK = 0b00011000
 GYRO_FS_SEL_250DPS = 0b00000000
 GYRO_FS_SEL_500DPS = 0b00001000
@@ -82,19 +85,21 @@ _I2C_BYPASS_MASK = 0b00000010
 _I2C_BYPASS_EN = 0b00000010
 _I2C_BYPASS_DIS = 0b00000000
 
-SF_G = 1
-SF_M_S2 = 9.80665 # 1 g = 9.80665 m/s2 ie. standard gravity
-SF_DEG_S = 1
-SF_RAD_S = 0.017453292519943 # 1 deg/s is 0.017453292519943 rad/s
+#  Units of measurements: conversion factors/selectors
+ACCEL_UNIT_G = 1
+ACCEL_UNIT_M_S2 = 9.80665 # 1 g = 9.80665 m/s2 ie. standard gravity
+GYRO_UNIT_DEG_S = 1
+GYRO_UNIT_RAD_S = 0.017453292519943 # 1 deg/s is 0.017453292519943 rad/s
 
 class MPU6500:
     """Class which provides interface to MPU6500 6-axis motion tracking device."""
     def __init__(
         self, i2c_interface=None, busnum=1, address=0x68,
-        accel_fs=ACCEL_FS_SEL_2G, gyro_fs=GYRO_FS_SEL_250DPS,
-        accel_sf=SF_M_S2, gyro_sf=SF_RAD_S,
+        accel_senitivity=ACCEL_FS_SEL_2G, gyro_sensitivity=GYRO_FS_SEL_250DPS,
+        accel_unit_factor=ACCEL_UNIT_M_S2, gyro_unit_factor=GYRO_UNIT_RAD_S,
         gyro_offset=(0, 0, 0)
     ):
+        # Configure I2C connection
         if i2c_interface is None:
             # Use pure python I2C interface if none is specified.
             import Adafruit_PureIO.smbus
@@ -103,16 +108,15 @@ class MPU6500:
             # Otherwise use the provided class to create an smbus interface.
             self.i2c = i2c_interface(busnum)
 
-        #self.i2c = i2c
         self.address = address
 
         if 0x71 != self.read_whoami():
-            raise RuntimeError("MPU6500 not found in I2C bus.")
+            raise RuntimeError("MPU6500 not found on I2C bus.")
 
-        self._accel_so = self._accel_fs(accel_fs)
-        self._gyro_so = self._gyro_fs(gyro_fs)
-        self._accel_sf = accel_sf
-        self._gyro_sf = gyro_sf
+        self._accel_sens_f = self._compute_accel_sens_f(accel_senitivity)
+        self._gyro_sens_f = self._compute_gyro_sens_f(gyro_sensitivity)
+        self._accel_unit_f = accel_unit_factor
+        self._gyro_unit_f = gyro_unit_factor
         self._gyro_offset = gyro_offset
 
         # Enable I2C bypass to access for MPU9250 magnetometer access.
@@ -123,27 +127,34 @@ class MPU6500:
 
     def read_acceleration(self):
         """
-        Acceleration measured by the sensor. By default will return a
-        3-tuple of X, Y, Z axis acceleration values in m/s^2 as floats. Will
-        return values in g if constructor was provided `accel_sf=SF_M_S2`
-        parameter.
+        Acceleration measured by the sensor. Will return a 3-tuple of 
+        X, Y, Z axis acceleration values as floats. 
+        
+        By default the units are m/s^2. Will return values in g if 
+        constructor is provided with the parameter 
+        `accel_unit_factor=ACCEL_UNIT_G`.
         """
-        so = self._accel_so
-        sf = self._accel_sf
+        so = self._accel_sens_f
+        sf = self._accel_unit_f
 
-        xyz = self._read_register_three_shorts(_ACCEL_XOUT_H)
-        return tuple([value / so * sf for value in xyz])
+        xyz_raw = self._read_register_three_shorts(_ACCEL_XOUT_H)
+        return tuple([value / so * sf for value in xyz_raw])
 
     def read_gyro(self):
         """
-        X, Y, Z radians per second as floats.
+        Angular velocity measured by the sensor. Will return a 3-tuple of 
+        angular velocities around the X, Y, Z axes as floats. 
+        
+        By default the units are rad/s. Will return values in deg/s if 
+        constructor is provided with the parameter 
+        `gyro_unit_factor=GYRO_UNIT_DEG_S`.
         """
-        so = self._gyro_so
-        sf = self._gyro_sf
+        so = self._gyro_sens_f
+        sf = self._gyro_unit_f
         ox, oy, oz = self._gyro_offset
 
-        xyz = self._read_register_three_shorts(_GYRO_XOUT_H)
-        xyz = [value / so * sf for value in xyz]
+        xyz_raw = self._read_register_three_shorts(_GYRO_XOUT_H)
+        xyz = [value / so * sf for value in xyz_raw]
 
         xyz[0] -= ox
         xyz[1] -= oy
@@ -196,7 +207,7 @@ class MPU6500:
     def _write_register_char(self, register, value):
         self.i2c.write_byte_data(self.address, register, value)
 
-    def _accel_fs(self, value):
+    def _compute_accel_sens_f(self, value):
         self._write_register_char(_ACCEL_CONFIG, value)
 
         # Return the sensitivity divider
@@ -209,7 +220,7 @@ class MPU6500:
         elif ACCEL_FS_SEL_16G == value:
             return _ACCEL_SO_16G
 
-    def _gyro_fs(self, value):
+    def _compute_gyro_sens_f(self, value):
         self._write_register_char(_GYRO_CONFIG, value)
 
         # Return the sensitivity divider
